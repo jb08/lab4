6863744b09ffbef0f439064a5e0ecf28143b20d6
Jason Brown <jasonkingsley.brown@gmail.com>
2016-04-18 16:08:09 -0500

    changed day; added a few comments

--- src/lab4.cpp
+++ src/lab4.cpp
@@ -1,10 +1,14 @@
 #include "eecs230.h"
 
 struct Date {
+
+    //member variables
     int year;
     int month;
     int day;
     bool isAD;
+
+    //constructor
     Date(int y, int m, int d, bool AD)
     {
         year = y;
@@ -12,13 +16,14 @@ struct Date {
         day = d;
         isAD = AD;
     };
+
 };
 
 int main()
 try
 {
-    Date today(2016,1,27,true);
-    cout << today.day;
+    Date today(2016,4,20,true);
+    cout << "It is the " << today.day << "th day of the month.\n";
 }
 
 catch (runtime_error& e) {
